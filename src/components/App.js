d90236df9696b9e094f7f2b0b388475d1b5977f1
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-08 17:38:50 +0100

    Add Product description

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProductImage from './ProductImage';
 import ThumbnailList from './ThumbnailList';
+import ProductDescription from './ProductDescription';
 class App extends React.Component {
     render() {
         return (
             <div>
-                <ProductImage />
+                <div className="card-product">
+                    <ProductImage />
+                    <ProductDescription />
+                </div>
                 <ThumbnailList />
             </div>
         )
---
+++ src/components/ProductDescription.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import React from 'react';
+class ProductDescription extends React.Component {
+    render() {
+        return (<div>
+            <h5>Sneaker Company</h5>
+            <h1>Fall Limited Edition Sneakers</h1>
+            <p>These low profile sneakers are you perfect casual wear companion.
+            Featuring a durable rubber outer sole, they'll whistand everything the weather 
+            can offer</p>
+        </div>
+    )
+    }
+
+}
+
+export default ProductDescription;
