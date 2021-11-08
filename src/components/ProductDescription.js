9da13b2eb78837794fc2235b9dd17989a6b8e382
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-08 19:49:33 +0100

    styling description

--- src/components/ProductDescription.js
+++ src/components/ProductDescription.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import React from 'react';
 class ProductDescription extends React.Component {
     render() {
-        return (<div>
+        return (<div className="product-description">
             <h5>Sneaker Company</h5>
             <h1>Fall Limited Edition Sneakers</h1>
             <p>These low profile sneakers are you perfect casual wear companion.
