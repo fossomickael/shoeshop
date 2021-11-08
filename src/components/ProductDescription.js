712795296c024d923a997de0f97ec42f93b60f5d
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-08 17:45:06 +0100

    finish description

--- src/components/ProductDescription.js
+++ src/components/ProductDescription.js
@@ -8,6 +8,9 @@ class ProductDescription extends React.Component {
             <p>These low profile sneakers are you perfect casual wear companion.
             Featuring a durable rubber outer sole, they'll whistand everything the weather 
             can offer</p>
+            <p>$125.00 50%</p>
+            <p>$250</p>
+            <p>- 0 +<button>Add to cart</button></p>
         </div>
     )
     }
