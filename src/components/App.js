1103b6be1eea0040eea68e3d93f34883f2ddb9e4
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-11 11:22:50 +0100

    design navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ProductImage from './ProductImage';
 import ThumbnailList from './ThumbnailList';
+import Navbar from './Navbar';
 import ProductDescription from './ProductDescription';
 class App extends React.Component {
     render() {
         return (
-            <div className="useful-width">
-                <div className="card-product">
-                    <div className="product-images">
-                        <ProductImage />
-                        <ThumbnailList />
-                    </div>
-                    <ProductDescription />
-                </div>
+            <div>
                 
+                <div className="useful-width">
+                    <Navbar />
+                    <div className="card-product">
+                        <div className="product-images">
+                            <ProductImage />
+                            <ThumbnailList />
+                        </div>
+                        <ProductDescription />
+                    </div>
+                    
+                </div>
             </div>
         )
     }
