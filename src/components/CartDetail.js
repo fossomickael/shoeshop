6b3e2b0d48f4eae600397751f215bfbb04161b00
Mickael FOSSO <fosso.mickael@gmail.com>
2021-11-12 11:01:03 +0100

    cart detail visible when clicking

--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -1,18 +1,28 @@
 import React from 'react';
-
-
+import { connect } from 'react-redux';
+import CartLine from './CartLine';
 class CartDetail extends React.Component {
 
 
  
     render() {
         return (
-            <div className="thumblist">
-                Details du panier
+            <div className="cart-detail">
+                Cart
+                <hr />
+                {this.props.cart.map(item => {
+                    return <CartLine itemId={item.id} key={item.id} />
+                })}
+                <button className="btn-checkout">Checkout</button>
             </div>
         )
     }
 
 }
 
-export default CartDetail;
+const mapStateToProps = state => {
+    return { cart: state.cart };
+};
+
+
+export default connect( mapStateToProps)(CartDetail);
